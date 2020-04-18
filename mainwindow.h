cf13f42c86339a2777f6041a833cdda9301e36e7
Magnus07 <stanislav.pinchuk.02@gmail.com>
2020-04-18 16:07:21 +0300

    Project init

---
+++ mainwindow.h
@@ -0,0 +1,54 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <math.h>
+#include <stack>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_6_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    int calculate(int x, int y, QString action);
+
+    void buildTable();
+
+    QStringList getOrder();
+
+    void fillVariables(int rows, int column, int multiplier, int variables);
+
+    bool moveToOutput(QString stack, QString current);
+};
+
+#endif // MAINWINDOW_H
